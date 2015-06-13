53b013907f79f19a4a75ed3aa223e9b6910c70be
aboelleef <a.aboelllef@gmail.com>
2015-06-13 20:26:01 +0200

    add final touches on fourth upwork project and add second material action deom

---
+++ upwork/fourth/js/base.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#drop-list").hide();
+	$("#drop-list").css("left",$("#drop-toggler").offset().left-40);
+	$("#drop-list").css("top",$("#drop-toggler").offset().top+70);
+
+	$("#drop-toggler").click(function(){
+		$("#drop-list").toggle(250);
+	});
+
+	
+});
