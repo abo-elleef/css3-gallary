17175f93ec3622eb409a02b5b7ff68fb7785f1b9
aboelleef <a.aboelllef@gmail.com>
2015-06-15 19:14:27 +0200

    add table sumer for page 2 fourth upwork project

--- upwork/fourth/js/base.js
+++ upwork/fourth/js/base.js
@@ -7,5 +7,18 @@ $(document).ready(function(){
 		$("#drop-list").toggle(250);
 		return false;
 	});
+
+	var updateTableCounters = function(){
+		var table = $(this).parents("table");
+		var rows = $(table).find("tr");
+		var total = 0;
+		for (var i = 1; i < rows.length; i++) {
+			var count = $(rows[i]).find("td").last().find("input").val();
+			total = total + parseInt(count) ;
+		};
+		$(table).find("tr").first().find("th").last().html(total)
+	};
+
+	$(".count").change(updateTableCounters);
 	
 });
