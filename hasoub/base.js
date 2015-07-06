79695e5962e6ff82cc450edf6634c2063558d4ae
aboelleef <a.aboelllef@gmail.com>
2015-07-06 04:16:42 +0200

    add hasoub slider

--- hasoub/base.js
+++ hasoub/base.js
@@ -4,9 +4,65 @@ var right = function(container){
 var left = function(container){
   container.addClass('left');
 };
+var getFirstThumb = function(){
+	return $(".thumb").first();
+}
+var getLastThumb = function(){
+	return $(".thumb").last();
+};
+var getNext = function(){
+	var current = $(".thumb.active");
+	var next ;
+	if( current.length ){
+		next = $(current).next(".thumb");
+		if( next.length ){ return next; } else { return getFirstThumb().addClass("active") };
+	}else{
+		return getFirstThumb().addClass("active");
+	};
+};
+var getPrev = function(){
+	var current = $(".thumb.active");
+	var prev ;
+	if(current.length){
+		prev = $(current).prev(".thumb");
+		if( prev.length ){ return prev; } else { return getLastThumb().addClass("active") };
+	}else{
+		return getLastThumb().addClass("active");
+	}
+};
+var drawThumbs = function(pathes){
+	if(pathes.length < 2){
+		$("#thumbs").css("height",0);
+	};
+};
+var slide = function(that){
+	$(".img-container").find("img").attr("src",$(that).find("img").attr("src"));
+};
+var forword = function(){
+	var next = getNext();
+	$(".thumb.active").removeClass("active");
+	next.addClass("active");
+	slide(next);
+};
+var backword = function(){
+	var prev = getPrev();
+	console.log(prev);
+	$(".thumb.active").removeClass("active");
+	prev.addClass("active");
+	slide(prev);
+};
+$.fn.extend({
+	"msatSlider": function(){
+		var pathes = [];
+		var thumbs = $(this).find(".thumb");
+		console.log(typeof(thumbs));
+		for (var i = 0; i < thumbs.length; i++) {
+			pathes.push($(thumbs[i]).find("img").attr("src"));
+		};
+		drawThumbs(pathes);
+		$(".thumb").click(function(){slide(this)});
+	}
+});
 $("document").ready(function(){
-  $(".img-container").click(function(){
-    right($(this));
-  });
-
+  $("#thumbs").msatSlider()
 });
